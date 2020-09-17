661f38c5eafaccae83e76c32818ad1f56a5d9afc
szl6038 <szl6038@psu.edu>
2020-09-17 00:20:13 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,25 @@
+//Authoer 
+
+#include <stdio.h>
+#include<stdlib.h>
+#include<readline/readline.h>
+
+int n;
+
+int digit_sum(n) {
+  if (n <= 9){
+    return n;
+  }
+  else {
+    return (n%10 + digit_sum(n/10));
+  }
+}
+
+int main(void) {
+  
+  int number = atoi(readline("Enter an int: "));
+
+  printf("sum of digits of %d is %d.\n", number, digit_sum(number));
+
+  return 0;
+}
